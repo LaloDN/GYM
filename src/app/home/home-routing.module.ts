311ad32750760403193b1a707541285f890b7f75
LaloDN <lalorayado1@live.com.mx>
2021-05-25 21:39:57 -0500

    Gimnasio final

---
+++ src/app/home/home-routing.module.ts
@@ -0,0 +1,48 @@
+
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomePage } from './home.page';
+
+
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomePage,
+    children: [
+      {
+      path: 'calendar',
+      loadChildren: () =>
+      import('../pages/calendar/calendar.module').then(
+      m => m.CalendarPageModule
+      )
+      },
+      {
+      path: 'exercises',
+      loadChildren: () =>
+      import('../pages/exercises/exercises.module').then(
+      m => m.ExercisesPageModule
+      )
+      },
+      {
+      path: 'maps',
+      loadChildren: () =>
+      import('../pages/maps/maps.module').then(
+      m => m.MapsPageModule
+      )
+      },
+      {
+        path: 'help',
+        loadChildren: () =>
+        import('../pages/help/help.module').then(
+        m => m.HelpPageModule
+        )
+        }
+      ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class HomePageRoutingModule {}
